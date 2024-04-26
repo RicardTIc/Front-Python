b8f0dca66fb079a1af0563f0faaec5e81b29daf4
Ricardo <cat-richard@hotmail.com>
2024-04-25 23:48:58 -0300

    atividade

---
+++ FEA-P11/NgRxV1a/src/app/store/contador.reducer.ts
@@ -0,0 +1,18 @@
+import { createReducer, on } from "@ngrx/store";
+import {incrementar, decrementar } from "./contador.actions";
+
+const estadoInicial = 0;
+
+export const contadorReducer = createReducer(
+    estadoInicial,
+    on(incrementar, (estado, action) => estado + action.valor),
+    on(decrementar, (estado, action) => estado - action.valor),
+);
+
+// //sintaxe alternativa
+// export function contadorReducer(estado = estadoInicial, action:ContadorActions) {
+//     if(action.type === '[Contador]Incrementar') {
+//         return estado + action.valor;
+//     }
+//     return estado;
+// }
